877c3f37c5990bd5acb8941a018b78d0074fbd7e
telpvn <telpvn@mail.ru>
2022-09-20 21:51:12 +0400

    second commit

--- src/scraping/apps.py
+++ src/scraping/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class ScrapingConfig(AppConfig):
     name = 'scraping'
+    verbose_name = 'Приложение по сбору вакансий'
+
--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Название населенного пункта',
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Название населенного пункта'
+        verbose_name_plural = 'Название населенных пунктов'
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Язык программирования',
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
